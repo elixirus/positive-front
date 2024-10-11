aa30e6cc415035ba09e0a035566d2ac69dea5644
elixirus <krondeveloper@gmail.com>
2024-10-11 18:38:54 +0300

    add blog

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -472,10 +472,10 @@ interface NavItem {
 }
 
 const NAV_ITEMS: Array<NavItem> = [
-    // {
-    //     label: 'blog',
-    //     href: 'https://blog.positive.com'
-    // },
+    {
+        label: 'blog',
+        href: 'https://blog.positive.com'
+    },
     {
         label: 'ctf',
         href: 'ctf'
